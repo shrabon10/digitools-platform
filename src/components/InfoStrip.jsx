bc2d4c6a010362b4ee5c118f7f0d8031f86a98af
shrabon10 <omarfaruksrabon@gmail.com>
2026-03-30 21:58:52 +0600

    Cart Section & Main Section Succesfully Funtionable

--- src/components/InfoStrip.jsx
+++ src/components/InfoStrip.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 
 const InfoStrip = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='primary-bg'>
+            <div className='my-10 container mx-auto'>
+                <div className='grid grid-cols-3 justify-center items-center gap-20 p-10 text-white text-center'>
+                    <div className='space-y-2 md:border-r border-r-white/70'>
+                        <h2 className='text-4xl font-extrabold'>50K+</h2>
+                        <p className='text-[#FFFFFF]/80'>Active Users</p>
+                    </div>
+                    <div className='space-y-2 md:border-r border-r-white/70'>
+                        <h2 className='text-4xl font-extrabold'>200+</h2>
+                        <p className='text-[#FFFFFF]/80'>Premium Tools</p>
+                    </div>
+                    <div className='space-y-2'>
+                        <h2 className='text-4xl font-extrabold'>4.9</h2>
+                        <p className='text-[#FFFFFF]/80'>Rating</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/components/TabSection.jsx
+++ src/components/TabSection.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
-
-const TabSection = () => {
+const TabSection = ({activeTab, setActiveTab, carts}) => {
+    
     return (
-        <div>
-            
+        <div className='mt-20 container mx-auto'>
+            <div className=' text-center space-y-4'>
+                <h2 className=' text-5xl font-extrabold'>Premium Digital Tools</h2>
+                <p className='p-3 md:p-0 max-w-[450px] mx-auto text-black/70 font-medium'>Choose from our curated collection of premium digital products designed to boost your productivity and creativity.</p>
+            </div>
+
+            <div className="mt-5 tabs tabs-box flex justify-center items-center gap-5 bg-transparent">
+                <input
+                    type="radio" name="my_tabs_1"
+                    className={`tab rounded-full px-7 text-lg font-bold ${activeTab === 'product' ? "primary-bg text-white" : "text-black"} `}
+                    aria-label="Products"
+                    onClick={() => setActiveTab("product")}
+                    defaultChecked />
+                <input
+                    type="radio"
+                    name="my_tabs_1"
+                    className={`tab rounded-full px-7 text-lg font-bold ${activeTab === 'cart' ? "primary-bg text-white" : "text-black"} `}
+                    aria-label={`Cart (${carts.length})`}
+                    onClick={() => setActiveTab("cart")} />
+            </div>
         </div>
     );
 };
